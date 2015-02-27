6d77b0329f6f4d3b725c7efdf2db9ba2c0ad31da
Thomas Heller <info@zilence.net>
2015-02-27 13:53:42 +0100

    fix autotest example so it doesn't conflict with lein cljs-dev

--- src/dev/build.clj
+++ src/dev/build.clj
@@ -8,24 +8,19 @@
       (cljs/enable-source-maps)
       (assoc :optimizations :none
              :pretty-print true
-             :work-dir (io/file "target/cljs-work")
-             :cache-dir (io/file "target/cljs-cache")
+             :work-dir (io/file "target/cljs-test-work")
+             :cache-dir (io/file "target/cljs-test-cache")
              :cache-level :jars
-             :public-dir (io/file "demo/js")
-             :public-path "demo/js")
+             :public-dir (io/file "target/cljs-test")
+             :public-path "cljs-test")
       (cljs/step-find-resources-in-jars)
       (cljs/step-find-resources "src/cljs")
       (cljs/step-find-resources "test/cljs")
-
       (cljs/step-finalize-config)
-
-      (cljs/step-configure-module :demo ['demo.app] #{})
 
       (cljs/watch-and-repeat!
         (fn [state modified]
           (-> state
-              (cljs/step-compile-modules)
-              (cljs/flush-unoptimized)
               (cond->
                 ;; first pass, run all tests
                 (empty? modified)
